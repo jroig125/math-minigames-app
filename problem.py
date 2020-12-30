e7824ee72c5c567e668420cea8926eb2170bc073
jroig125 <67767296+jroig125@users.noreply.github.com>
2020-12-30 13:51:42 -0500

    Updated answer list algorithm for more intentional incorrect answers; cleaned up answer appearance

--- problem.py
+++ problem.py
@@ -15,9 +15,10 @@ class Problem():
             return add_subtract_integers(self.difficulty)
 
     def __str__(self):
+        answer_labels = ['a', 'b', 'c', 'd', 'e']
         answer_text = ''
-        for answer in self.answers:
-            answer_text += f'{answer}\n'
+        for answer in range(len(self.answers)):
+            answer_text += f'{answer_labels[answer]}. {self.answers[answer]}\n'
 
         correct_answer_text = f'Correct answer: {self.correct_answer}\n'
 
@@ -26,9 +27,9 @@ class Problem():
 
 def generate_digit(difficulty):
     if difficulty == "easy":
-        return random.randint(0, 9)
+        return random.randint(-5, 9)
     if difficulty == "medium":
-        return random.randint(0, 25)
+        return random.randint(-9, 25)
     if difficulty == "hard":
         return random.randint(-20, 50)
     else:
@@ -44,30 +45,33 @@ def add_subtract_integers(difficulty):
     return problem, answers, correct_answer
 
 
+# asi = add/subtract integers
 def asi_answer_gen(digit_1, digit_2, correct_answer):
     # intentionally incorrect answers
     subtract_ans = digit_1 - digit_2
     flip_subtract_ans = digit_2 - digit_1
     negative_ans = -correct_answer
+    add_one_ans = correct_answer + 1
+    sub_one_ans = correct_answer - 1
 
-    answers = {subtract_ans, flip_subtract_ans, negative_ans}
+    intentional_answers = {subtract_ans, flip_subtract_ans, negative_ans, add_one_ans, sub_one_ans}
 
     # all one more or one less than one of the intentionally incorrect answers
     distract_answers = set()
-    for answer in answers:
+    for answer in intentional_answers:
         distract_answers.add(answer + 1)
         distract_answers.add(answer - 1)
 
-    # TODO: Update so that likelihood of intentionally incorrect answer is much higher than
-    # that of a distraction
+    # likelihood for intentional incorrects is 75%; 25% for other distractions
+    answers = set()
+    answers.add(correct_answer)
+    while len(answers) < 5:
+        if random.random() < 0.75 and len(intentional_answers) > 0:
+            answers.add(intentional_answers.pop())
+        else:
+            answers.add(distract_answers.pop())
 
-    # final answer set is assortment of intentional incorrects and distraction answers
-    answers = list(answers.union(distract_answers))
-    random.shuffle(answers)
-
-    # final answer set has 4 incorrect options and 1 correct option
-    answers = answers[0:4]
-    answers.append(correct_answer)
+    answers = list(answers)
     random.shuffle(answers)
 
     return answers
@@ -76,10 +80,12 @@ def asi_answer_gen(digit_1, digit_2, correct_answer):
 class DifficultyNotDefinedError(BaseException):
     pass
 
+
 def main():
     difficulties = ['easy', 'medium', 'hard']
     for difficulty in difficulties:
         print(Problem(difficulty, 'Adding and Subtracting Integers'))
+
 
 if __name__ == "__main__":
     main()
